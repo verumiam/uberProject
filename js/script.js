e21ff17227442b42afc35df8d02afc691c868a5e
Danil Grigoruk <danil_grigoruk@mail.ru>
2022-07-22 16:53:19 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    // Hamburger Menu
+    const menu = document.querySelector('.menu'),
+          menuItem = document.querySelectorAll('.menu_item'),
+          hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    });
+
+    // Slider cars
+    
+    const slides = document.querySelectorAll('.slider_item'),
+        prev = document.querySelector('.slider_arrows_prev'),
+        next = document.querySelector('.slider_arrows_next');
+
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
+});
